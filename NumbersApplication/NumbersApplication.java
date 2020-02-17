ceed1d36f2324deff3c87a40a2f481a0d07a5746
root <root@localhost.localdomain>
2020-02-17 01:41:17 -0500

    Numbers application final{needs comments}

--- NumbersApplication/NumbersApplication.java
+++ NumbersApplication/NumbersApplication.java
@@ -41,48 +41,67 @@ public class NumbersApplication{
            if(isPrime)
            {
                millionPrimes.add(i);
+               //System.out.println("Prime: [%d]", i);
                count+=1;
            }
         }
         //Check all pair of primes and enter that to a list of ints
         ArrayList<Integer> pairOfPrime = pairPrimes(millionPrimes);
-        //Check for all perfect nums between 1 and 10000
-        ArrayList<Integer> perfectNumbers = perfectNums(10000);
-        final long endTime = System.currentTimeMillis();
+        //Check for all perfect nums between 1 and 1000
+        ArrayList<Integer> perfectNumbers = perfectNums(1000);
         //print out all prime pairs
         //create numPairs to count pairs
         int numPair = 0;
         for(int i = 0; i < pairOfPrime.size(); i+=2)
         {
            numPair++;
-           System.out.printf("Pair %d is %d and %d%n", numPair, pairOfPrime.get(i), pairOfPrime.get(i+1));
+         //  System.out.printf("Pair %d is %d and %d%n", numPair, pairOfPrime.get(i), pairOfPrime.get(i+1));
         }
         //print all perfect nums
+        int perfectNumsCount = 0;
         for(int i = 0; i < perfectNumbers.size();i++)
         {
-           System.out.printf("%d is a perfect number%n", perfectNumbers.get(i));
-        }
-        //print cube digits up to 10000(dosent work past 500
-        for(int i = 1; i < 10000; i++){
-            boolean isCube = cubeDigit(i);
-            if(isCube)
-                System.out.printf("The cube of all the digits in %d is itself%n" , i);
+           // System.out.printf("%d is a perfect number%n", perfectNumbers.get(i));
+            perfectNumsCount++;
         }
         //print square nums
+        int squareNumCount = 0;
         for(int i = 1000; i < 10000; i++){
             boolean isSquareNum = squareSum(i);
             if(isSquareNum)
-                System.out.printf("The square sum of the first 2 and the last 2 digits in %d is  == %d%n", i, i);
+                // System.out.printf("The square sum of the first 2 and the last 2 digits in %d is  == %d%n", i, i);
+                squareNumCount++;
         }
         // print all nums that are abundant or deficent between 1-10000
+        int abundant = 0;
+        int deficent = 0;
         for(int i = 1; i<=140; i++){
             int x = abundantOrDeficient(i);
             if(x == 1)
-                System.out.printf("[+]%d is abundant%n", i);
+                //System.out.printf("[+]%d is abundant%n", i);
+                abundant++;
             else if(x == -1)
-                System.out.printf("[-]%d is deficent%n", i);
+                //System.out.printf("[-]%d is deficent%n", i);
+                deficent++;
         }
+        int cubeSum = 0;
+        //print cube digits up to 1000
+        for(int i = 1; i < 1000; i++){
+            boolean isCube = cubeDigit(i);
+            if(isCube)
+                // System.out.printf("The cube of all the digits in %d is itself%n" , i);
+                cubeSum++;
+        }
+
         System.out.printf("There are %d prime numbers in 1 MILLION%n", count);
+        System.out.printf("There are %d pairs of prime numbers where the prime number *2 plus 1 is also prime in 1 MILLION%n", numPair);
+        System.out.printf("There are %d perfect numbers in 1 thousand%n", perfectNumsCount);
+        System.out.printf("There are %d abundant numbers and %d deficent numbers in one thousand%n", abundant, deficent);
+        System.out.printf("The cube of all the digits in a number equalls the number %d time between 1-1000%n",cubeSum);
+        System.out.printf("There are %d nums where the square sum of the first 2 digits and the last 2 digits == the starting num%n", squareNumCount);
+
+
+        final long endTime = System.currentTimeMillis();
         System.out.println("Total execution time: " + (endTime - startTime));
     }
 
