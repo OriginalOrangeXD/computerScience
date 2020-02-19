aa370e475b3fe31fa2f8bff9a3071ecdeb53607b
Robert <deangelis.robert13@gmail.com>
2020-02-19 09:02:29 -0500

    Min and Max function

--- NumberFunctions/NumberFunctions.java
+++ NumberFunctions/NumberFunctions.java
@@ -1,5 +1,32 @@
 public class NumberFunctions
 {
+    public static int min(int a, int b)
+    {
+        if (a > b) return b;
+        return a;
+    }
+    public static float min(float a, float b)
+    {
+        if (a > b) return b;
+        return a;
+    }
+    public static double min(double a, double b)
+    {
+        if (a > b) return b;
+        return a;
+    }
+    public static int min(int[] arrayOfInts)
+    {
+        int worstScore = Integer.MAX_VALUE;
+        for(int i = 0; i < arrayOfInts.length; i++)
+        {
+            if(arrayOfInts[i] < worstScore)
+            {
+                worstScore = arrayOfInts[i];
+            }
+        }
+        return worstScore;
+    }
     public static int max(int a, int b)
     {
         if (a < b) return b;
@@ -33,6 +60,9 @@ public class NumberFunctions
             System.out.printf("The max between 5 and 10 is %d%n", max(5, 10));
             System.out.printf("The max between 5.89 and 6.25 is %f%n", max(5.89, 6.25));
             System.out.printf("The max between 6, 100, 8, 9, and 98 is %d%n", max(numArry));
+            System.out.printf("The min between 5 and 10 is %d%n", min(5, 10));
+            System.out.printf("The min between 5.89 and 6.25 is %f%n", min(5.89, 6.25));
+            System.out.printf("The min between 6, 100, 8, 9, and 98 is %d%n", min(numArry));
 
     }
 }
