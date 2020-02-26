154c447d0483c0b044604546f93c22248f0b09d4
Robert <deangelis.robert13@gmail.com>
2020-02-25 19:38:10 -0500

    testing java

--- recursion/recursion.java
+++ recursion/recursion.java
@@ -4,28 +4,51 @@ public class recursion
     public static long factTotal = 1;
     public static long powerTotal;
 
-    public static long power(long x, long n)
+    /**
+     * This power method returns the value of the first input to the power of the second
+     * @param base long base input
+     * @param exponent long exponent input
+     * @return the value of the base to the power of the exponent
+     */
+    public static long power(long base, long exponent)
     {
-        if(n == 0) return 1;
-        powerTotal += power(x, n-1) * x;
+        if(exponent == 0) return 1;
+        powerTotal += power(base, exponent-1) * base;
         return powerTotal;
     }
 
 
-    public static long combonations(long n, long r)
+    /**
+     * This combonations method will find all unique permutations with a set size
+     * @param setSize long input of the amount of objects
+     * @param permSize long input of the permutation size
+     * @return the amount of unique permutations
+     */
+    public static long combonations(long setSize, long permSize)
     {
-        if(n == r || r == 0||r > n) return 1;
-        combo = combonations(n-1,r) + combonations(n-1, r-1);
+        if(setSize == permSize || permSize == 0||permSize > setSize) return 1;
+        combo = combonations(setSize-1,permSize) + combonations(setSize-1, permSize-1);
         return combo;
     }
 
-    public static long gcd(long m, long n)
+    /**
+     * This gcd method will find the greatest common denominator between 2 numbers
+     * @param num1 long input that is not zero
+     * @param num2 long input that is not zero
+     * @return the greatest common denominator
+     */
+    public static long gcd(long num1, long num2)
     {
-        if(n == 0) return m;
-        return gcd(n, m%n);
+        if(num2 == 0) return num1;
+        return gcd(num2, num1%num2);
 
     }
 
+    /**
+     * This factorial method will return the factorial
+     * @param x long input to find the factorial of the number
+     * @return the long of the factorial of the number
+     */
     public static long factorial(long x)
     {
         if(x <= 0) return factTotal;
@@ -33,18 +56,24 @@ public class recursion
         return factorial(x-1);
     }
 
-    public static long fibonacci(long x)
+
+    /**
+     * This fibonacci method will find a given position inthe fibonacci sequence
+     * @param pos long input to identify the position
+     * @return the number in that position of the fibbonacci sequence
+     */
+    public static long fibonacci(long pos)
     {
-        if(x <=2) return 1;
-        return fibonacci(x-1) + fibonacci(x-2);
+        if(pos <=2) return 1;
+        return fibonacci(pos-1) + fibonacci(pos-2);
     }
 
     public static void main(String[] args)
     {
-        System.out.println(combonations(4, 2));
+        System.out.println(combonations(5, 2));
         System.out.println(factorial(4));
         System.out.println(power(4, 6));
-        System.out.println(gcd(7,6));
+        System.out.println(gcd(4096,8192));
         System.out.println(fibonacci(13));
     }
 }
