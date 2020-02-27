5adc05928a5f76a8a6ca2398a0167b6d6fc392ae
root <root@localhost.localdomain>
2020-02-27 11:52:37 -0500

    roman numerals

---
+++ romanNumerals/roman.java
@@ -0,0 +1,31 @@
+public class roman
+{
+    public static int values(char romanNumeral)
+    {
+       if(romanNumeral == 'I') return 1;
+       if(romanNumeral == 'V') return 5;
+       if(romanNumeral == 'X') return 10;
+       if(romanNumeral == 'L') return 50;
+       if(romanNumeral == 'C') return 100;
+       if(romanNumeral == 'D') return 500;
+       if(romanNumeral == 'M') return 1000;
+       else return 0;
+    }
+    public static int convertToInt(String romanNumeral)
+    {
+       int total = 0;
+       for(int i = 0; i < romanNumeral.length(); i++)
+       {
+          int value1 = values(romanNumeral.charAt(i);
+       }
+       return total;
+    }
+    public static String convertToString(int a)
+    {
+        return "Hello";
+    }
+    public static void main(String[] args)
+    {
+        System.out.printf("The int of the roman numberal of X is %d%n", convertToInt("X"));
+    }
+}
